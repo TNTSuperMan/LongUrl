7e91962867a523f691b2e995e9b0f9c7642a4105
TNTSuperMan <tntsuperga@gmail.com>
2024-12-10 22:47:38 +0900

    読込機能

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,24 @@ import './assets/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-
-app.mount('#app')
+if(location.hash){
+  try{
+    const json:unknown = JSON.parse(atob(decodeURIComponent(location.hash.substring(1))))
+    if(json instanceof Array){
+      if(json.length < 2) throw 0;
+      const padding: number = json[0];
+      const content: string = json[1];
+      let url = "";
+      for(let i = 0;i < content.length;i++)
+        i % padding == 0 ? (url += content[i]) : 0;
+    }else{
+      throw 0;
+    }
+    
+    console.log(json)
+  }catch{
+    location.href = "https://bing.com"
+  }
+}else{
+  createApp(App).mount('#app')
+}
